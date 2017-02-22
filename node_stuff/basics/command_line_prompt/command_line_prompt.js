368616437bfb66a6f2be22ca6152365fd80f83b1
Charles Gates <gatesc@us.ibm.com>
2017-02-22 18:22:24 -0500

    added an example to prompt the user for information

---
+++ node_stuff/basics/command_line_prompt/command_line_prompt.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env nodejs
+
+/* This is to demonstrate how to use the prompting */
+
+(function() {
+    'use strict';
+     var prompt = require('prompt');
+     prompt.start();
+
+     var basicPrompt = function(callback) {
+       /******************************************
+       * Let's prompt for a username and email
+       ******************************************/
+       prompt.get(['username', 'email'], function(err, theResult) {
+            if (!err) {
+                console.log('The username and email are...');
+                console.log('   username:  ' + theResult.username);
+                console.log('   email:     ' + theResult.email);
+            }
+            else {
+                console.log('An error occurred');
+            }
+            return callback();
+       });
+    }
+
+    var pwPrompt = function(callback) {
+       /******************************************
+       *  example prompting for a password
+       ******************************************/
+       // need to set a property to hide the typed characters
+       var properties = [
+         {
+            name: 'username'
+         },
+         {
+            name: 'password',
+            hidden: true
+         }
+       ];
+       prompt.get(properties, function(err, theResult) {
+         if (!err) {
+            console.log('The username and password are...');
+            console.log('    username:   ' + theResult.username);
+            console.log('    password:   ' + theResult.password);
+         }
+         else {
+            console.log('An error occurred');
+         }
+         return callback();
+       });
+    }
+
+    var main = function() {
+        basicPrompt(function(err, theResult) {
+                console.log("done with basic prompt");
+                pwPrompt(function(err, theResult) {
+                        console.log("done with pw prompt");
+                    }
+                );
+            }
+        )
+
+    }
+
+    main();
+  }
+)();
