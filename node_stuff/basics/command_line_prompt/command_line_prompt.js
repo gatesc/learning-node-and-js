15138bec33cce09af4a616b3741c6c468b5e005f
Charles Gates <charles.gates@gmail.com>
2017-02-22 19:24:17 -0500

    fixed callback arguments

--- node_stuff/basics/command_line_prompt/command_line_prompt.js
+++ node_stuff/basics/command_line_prompt/command_line_prompt.js
@@ -4,14 +4,14 @@
 
 (function() {
     'use strict';
-     var prompt = require('prompt');
-     prompt.start();
+    var prompt = require('prompt');
+    prompt.start();
 
-     var basicPrompt = function(callback) {
-       /******************************************
-       * Let's prompt for a username and email
-       ******************************************/
-       prompt.get(['username', 'email'], function(err, theResult) {
+    var basicPrompt = function(callback) {
+	/******************************************
+	 * Let's prompt for a username and email
+	 ******************************************/
+	prompt.get(['username', 'email'], function(err, theResult) {
             if (!err) {
                 console.log('The username and email are...');
                 console.log('   username:  ' + theResult.username);
@@ -20,49 +20,38 @@
             else {
                 console.log('An error occurred');
             }
-            return callback();
-       });
+            return callback(err, theResult);
+	});
     }
 
     var pwPrompt = function(callback) {
-       /******************************************
-       *  example prompting for a password
-       ******************************************/
-       // need to set a property to hide the typed characters
-       var properties = [
-         {
-            name: 'username'
-         },
-         {
-            name: 'password',
-            hidden: true
-         }
-       ];
-       prompt.get(properties, function(err, theResult) {
-         if (!err) {
-            console.log('The username and password are...');
-            console.log('    username:   ' + theResult.username);
-            console.log('    password:   ' + theResult.password);
-         }
-         else {
-            console.log('An error occurred');
-         }
-         return callback();
-       });
+	/******************************************
+	 *  example prompting for a password
+	 ******************************************/
+	// need to set a property to hide the typed characters
+	var properties = [{name: 'username'}, {name: 'password', hidden: true}];
+	prompt.get(properties, function(err, theResult) {
+            if (!err) {
+		console.log('The username and password are...');
+		console.log('    username:   ' + theResult.username);
+		console.log('    password:   ' + theResult.password);
+            }
+            else {
+		console.log('An error occurred');
+            }
+            return callback(err, theResult);
+	});
     }
 
     var main = function() {
         basicPrompt(function(err, theResult) {
-                console.log("done with basic prompt");
-                pwPrompt(function(err, theResult) {
-                        console.log("done with pw prompt");
-                    }
-                );
-            }
-        )
-
+            console.log("done with basic prompt");
+            pwPrompt(function(err, theResult) {
+                console.log("done with pw prompt");
+            });
+        })
     }
 
     main();
-  }
+}
 )();
