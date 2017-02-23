91abc0f32027b9d2b2965b72182218eecb505fba
Charles Gates <charles.gates@gmail.com>
2017-02-22 19:14:02 -0500

    moving fiels around

--- node_stuff/basics/reading_a_file/reading_a_file.js
+++ node_stuff/basics/reading_a_file/reading_a_file.js
@@ -1,8 +1,18 @@
+#!/usr/bin/env nodejs
+
 /* Code to read a file via node.js and display it to the screen
    Used to demonstrate (1) how to read the file and (2) using
    node.js callback mechanism
 */
 
-var fs = require('fs');
+(function() {
+    "use strict";
+    var fs = require('fs');
+    var inputFile = "input.txt";
+
+    /* trivial method */
+    var theData = fs.readFile(inputFile);
+    console.log(theData);
+})();
 
 
